81a42d41d9ebe17925dadef387001233011eda53
Yasserghonems <yasserghonem56@gmail.com>
2023-04-19 15:46:18 -0700

    hot fix

--- shell.h
+++ shell.h
@@ -13,9 +13,9 @@
 #include <signal.h> /* for signal management */
 #include <fcntl.h> /* for open files*/
 
-/************* MACROS **************/
+/************* MiCROS **************/
 
-#include "macros.h" /* for msg help and prompt */
+#include "micros.h" /* for msg help and prompt */
 
 /************* STRUCTURES **************/
 
